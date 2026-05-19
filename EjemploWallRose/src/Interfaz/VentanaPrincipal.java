fd0b004cb756f6fc47311d4c5afff7cfc27c8498
josedmz-cmd <josedaniel.morazunigagames@gmail.com>
2026-05-19 15:34:43 -0600

    Add files via upload

--- EjemploWallRose/src/Interfaz/VentanaPrincipal.java
+++ EjemploWallRose/src/Interfaz/VentanaPrincipal.java
@@ -5,6 +5,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
 import javax.swing.JTabbedPane;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VentanaPrincipal {
 
@@ -45,13 +49,60 @@ public class VentanaPrincipal {
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		frame.getContentPane().add(tabbedPane);
 		
-		JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.addTab("New tab", null, tabbedPane_1, null);
+		JPanel panelClientes = new JPanel();
+		tabbedPane.addTab("Clientes", null, panelClientes, null);
+		panelClientes.setLayout(null);
 		
-		JTabbedPane tabbedPane_2 = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.addTab("New tab", null, tabbedPane_2, null);
+		JButton BotonVisualizar_C = new JButton("Ver");
+		BotonVisualizar_C.setBounds(443, 11, 89, 23);
+		panelClientes.add(BotonVisualizar_C);
 		
-		JTabbedPane tabbedPane_3 = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.addTab("New tab", null, tabbedPane_3, null);
+		JButton BotonAgregar_C = new JButton("Agregar");
+		BotonAgregar_C.setBounds(443, 56, 89, 23);
+		panelClientes.add(BotonAgregar_C);
+		
+		JButton BotonEditar_C = new JButton("Editar");
+		BotonEditar_C.setBounds(443, 111, 89, 23);
+		panelClientes.add(BotonEditar_C);
+		
+		JButton BotonBorrar_C = new JButton("Borrar");
+		BotonBorrar_C.setBounds(443, 165, 89, 23);
+		panelClientes.add(BotonBorrar_C);
+		
+		JPanel panelOrdenes = new JPanel();
+		tabbedPane.addTab("Ordenes", null, panelOrdenes, null);
+		panelOrdenes.setLayout(null);
+		
+		JButton BotonAgregar_O = new JButton("Nueva");
+		BotonAgregar_O.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		BotonAgregar_O.setBounds(453, 0, 89, 23);
+		panelOrdenes.add(BotonAgregar_O);
+		
+		JButton BotonVisualizar_O = new JButton("Detalle");
+		BotonVisualizar_O.setBounds(453, 55, 89, 23);
+		panelOrdenes.add(BotonVisualizar_O);
+		
+		JButton BotonBorrar_O = new JButton("Borrar");
+		BotonBorrar_O.setBounds(453, 102, 89, 23);
+		panelOrdenes.add(BotonBorrar_O);
+		
+		JPanel panelProductos = new JPanel();
+		tabbedPane.addTab("Productos", null, panelProductos, null);
+		panelProductos.setLayout(null);
+		
+		JButton BotonAgregar_P = new JButton("Agregar");
+		BotonAgregar_P.setBounds(443, 11, 89, 23);
+		panelProductos.add(BotonAgregar_P);
+		
+		JButton BotonEditar_P = new JButton("Editar");
+		BotonEditar_P.setBounds(443, 63, 89, 23);
+		panelProductos.add(BotonEditar_P);
+		
+		JButton BotonBorrar_P = new JButton("Borrar");
+		BotonBorrar_P.setBounds(443, 125, 89, 23);
+		panelProductos.add(BotonBorrar_P);
 	}
 }
